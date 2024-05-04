d101a96db0efc8fabc571a33c04bedd94e472e21
sakshi67 <77280231+sakshi67@users.noreply.github.com>
2024-05-04 09:57:24 +0100

    Update Ride.js

--- PRO-C69-ProjectTemplate-main/screens/Ride.js
+++ PRO-C69-ProjectTemplate-main/screens/Ride.js
@@ -15,7 +15,7 @@ export default class TransactionScreen extends Component {
   }
 
   getCameraPermissions = async domState => {
-    //const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
     //const { status } = Permissions.askAsync(Permissions.CAMERA);
     //const { status } = await Permissions.askAsync(Permissions);
     //const { status } = await Permissions.askAsync(CAMERA);
@@ -63,14 +63,14 @@ export default class TransactionScreen extends Component {
             <Text style={styles.buttonText}>Scan QR Code</Text>
           </TouchableOpacity> 
         */}
-        {/*
+        {
           <TouchableOpacity
             style={[styles.button, { marginTop: 25 }]}
             onPress={() => this.getCameraPermissions("scanner")}
           >
             <Text style={styles.buttonText}>Scan QR Code</Text>
           </TouchableOpacity> 
-        */}
+        }
         {/*
           <TouchableOpacity
             style={[styles.button, { marginTop: 25 }]}
